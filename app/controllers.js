4f9d4f5180f6c1833349eec79e55a920059a3669
Raymond Zhou <somecrazyidiot@gmail.com>
2013-05-01 13:14:55 -0400

    Move data outside of html.

--- app/controllers.js
+++ app/controllers.js
@@ -1,9 +1,25 @@
 app.controller( 'SpindleCtrl', [ '$scope', function( $scope ) {
   $scope.passages = [
     {
-      x: 0,
-      y: 0,
-      title:  'hello this is a title'
+      x: 400,
+      y: 200,
+      title:  'hello this is a title',
+      text: 'maximum horoscopes decide contemporary conundrums'
+    },
+    {
+      x: 200,
+      y: 150,
+      title: 'hello this is a second test',
+      text: 'how will this work?'
+    },
+    {
+      x: 30,
+      y: 50,
+      title: 'and here is a third test',
+      text: 'oh come on, we can think of better text than this'
     }
   ];
+
+  console.log( $scope.passages[0] );
 }]);
+
