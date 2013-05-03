2e2f925fa3cdded308ca17ce3ae4eb245856e16b
Raymond Zhou <somecrazyidiot@gmail.com>
2013-05-03 14:02:01 -0400

    Change footer. Work on getting editing to work with Pagedown (click to edit, ui stuff).

--- app/controllers.js
+++ app/controllers.js
@@ -1,34 +1,52 @@
 app.controller( 'SpindleCtrl', [ '$scope', function( $scope ) {
-  $scope.passages = [
-    {
-      x: 400,
-      y: 200,
-      width: 200,
-      title:  'hello this is a title',
-      text: 'maximum horoscopes decide contemporary conundrums'
-    },
-    {
-      x: 200,
-      y: 150,
-      title: 'hello this is a second test',
-      text: 'how will this work? [[displayed text|and here is a third test]]'
-    },
-    {
-      x: 30,
-      y: 50,
-      width: 150,
-      title: 'and here is a third test',
-      text: 'oh come on, we can think of better text than this'
-    },
-    {
-      x: 100,
-      y: 300,
-      width: 80,
-      title: 'how about a fourth?',
-      text: 'something new, and perhaps unexpected'
-    }
-  ];
+    $scope.passages = [
+      {
+        x: 400,
+        y: 200,
+        width: 200,
+        title:  'hello this is a title',
+        text: 'maximum horoscopes decide contemporary conundrums'
+      },
+      {
+        x: 200,
+        y: 150,
+        title: 'hello this is a second test',
+        text: 'how will this work? [[displayed text|and here is a third test]]'
+      },
+      {
+        x: 30,
+        y: 50,
+        width: 150,
+        title: 'and here is a third test',
+        text: 'oh come on, we can think of better text than this'
+      },
+      {
+        x: 100,
+        y: 300,
+        width: 80,
+        title: 'how about a fourth?',
+        text: 'something new, and perhaps unexpected'
+      }
+    ];
 
-  console.log( $scope.passages[0] );
-}]);
+    console.log( $scope.passages[0] );
+  }])
+  .controller( 'PassageCtrl', [ '$scope', function( $scope ) {
+    $scope.toggleTitleEditing = function() {
+      $scope.titleEditing = !$scope.titleEditing || false;
+      if ( $scope.textEditing ) {
+        $scope.textEditing = false;
+      }
 
+      console.log( 'toggle' );
+    };
+
+    $scope.toggleTextEditing = function() {
+      $scope.textEditing = !$scope.textEditing || false;
+      if ( $scope.titleEditing ) {
+        $scope.titleEditing = false;
+      }
+
+      console.log( 'toggleText' );
+    };
+  }]);
--- app/directives.js
+++ app/directives.js
@@ -74,11 +74,7 @@ app.directive( 'draggable', function( $document ) {
       require: '?ngModel',
 
       link: function( scope, element, attrs, ngModel ) {
-        element.bind( 'click', function( event ) {
-          // element.attr( 'contenteditable', 'true' );
-          console.log( element.val() );
-          console.log( converter.makeHtml( element.val() ) );
-        });
+        element.bind( 'click', function( event ) {});
 
         element.bind( 'mousedown', function( event ) {
           event.stopPropagation();
